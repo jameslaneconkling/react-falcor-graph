276501d7c895ca1599fb849b176b5ba7a9863499
James Conkling <james.lane.conkling@gmail.com>
2017-09-20 23:57:05 -0400

    export withState and withHandlers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 exports.connectFalcor = require('./connect').connectFalcor;
-exports.withGraphFragment = require('./withGraphFragment').default;
 exports.connectRedux = require('./connect').connectRedux;
+exports.withGraphFragment = require('./withGraphFragment').default;
 exports.withReduxStore = require('./withReduxStore').default;
+exports.withHandlers = require('./withHandlers').default;
+exports.withState = require('./withState').default;
