41385110f649574248846033e18984a397e78d51
James Conkling <james.lane.conkling@gmail.com>
2017-09-26 22:20:45 -0400

    fix mapValues arg order

--- src/withHandlers.js
+++ src/withHandlers.js
@@ -1,11 +1,11 @@
-const { mapHashMap } = require('./utils');
 require('rxjs/add/operator/map');
+const { mapValues } = require('./utils');
 
 
 exports.default = (handlers) => {
   let _props;
 
-  const cachedHandlers = mapHashMap(handlers, handler => (...args) => handler(_props)(...args));
+  const cachedHandlers = mapValues(handler => (...args) => handler(_props)(...args), handlers);
 
   return props$ => props$
     .map((props) => {
